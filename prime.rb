a86adb0d99058964da2cfc5c01eed343b73202a9
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 14:54:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 def prime?(number)
-  (2..(number-1)).each do |n|
-    return false if number % n == 0
+  if number.negative? || number < 0 || number == 1
+    false
+  else
+    (2..(number-1)).each do |n|
+      return false if number % n == 0
+    end
+    true
   end
-  true
 end
