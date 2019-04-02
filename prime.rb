2650d29551ae7b668508c3e8eaf068a622aae29a
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 14:48:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(number)
   if number >= 2
-    for x in 2..num do
-      if num % 1 == 0
+    for x in 2..number do
+      if number % 1 == 0
         return false
       else
         return true
